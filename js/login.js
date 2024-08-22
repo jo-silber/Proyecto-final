cb166332c9f14b1a3ccacfd4324c91c513bd8c40
emi <emilycabral185@gmail.com>
2024-08-22 15:10:46 -0300

    validacion al ingresar

---
+++ js/login.js
@@ -0,0 +1,16 @@
+document.getElementById('loginForm').addEventListener('submit', function(event) {
+    //evitar que se envie automaticamente
+    event.preventDefault();
+
+    //obtener los valores de los campos
+    const usuario = document.getElementById('usuario').value;
+    const contraseña = document.getElementById('contraseña').value;
+
+    //validar que no esten vacios
+    if (usuario === "" || contraseña === "") {
+        alert("Por favor, completa todos los campos.");
+    } else {
+        //redirigir a la pagina principal
+        window.location.href = "index.html";
+    }
+});
