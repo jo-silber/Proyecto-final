c911e38e902b46659809ab07f86a5f7e05f9ff49
emi <emilycabral185@gmail.com>
2024-09-14 19:52:01 -0300

    desafiate

--- js/products.js
+++ js/products.js
@@ -1,23 +1,93 @@
-document.addEventListener("DOMContentLoaded", function (e) {
-  let categoryId = 101;
-  let urlWithCategory = PRODUCTS_URL + categoryId + EXT_TYPE; // declaramos estas variables ya que el URL esta incompleto, y hay que llenarlo con la categoria que necesitamos.
+document.addEventListener('DOMContentLoaded', function (e) {
 
-  getJSONData(urlWithCategory).then(function (resultObj) {
-    if (resultObj.status === "ok") {
-      let productsArray = resultObj.data.products;
-      console.log(productsArray);
-      showProductsList(productsArray);
-    } else {
-      console.log("Error de productos");
+    let categoryId = 101;
+    let urlWithCategory = PRODUCTS_URL + categoryId + EXT_TYPE; // declaramos estas variables ya que el URL esta incompleto, y hay que llenarlo con la categoria que necesitamos.
+
+    let productsArray = [];
+
+    getJSONData(urlWithCategory).then(function (resultObj) {
+        if (resultObj.status === "ok") {
+            productsArray = resultObj.data.products;
+            console.log(productsArray);
+            showProductsList(productsArray);
+        
+        /* === FILTROS === */
+/* por precio */
+document.getElementById('filtrarPrecio').addEventListener('click', () =>{
+    let precioMin = parseFloat(document.getElementById('precioMin').value);
+    let precioMax = parseFloat(document.getElementById('precioMax').value);
+
+    let productosFiltrados = productsArray.filter(product => {
+        // verifico si lo que esta ingresando es un numero
+    let precioMinEsValido = true;
+    if(!isNaN(precioMin)){
+       precioMinEsValido = product.cost >= precioMin;
     }
-  });
+
+    let precioMaxEsValido = true;
+    if(!isNaN(precioMax)){
+        precioMaxEsValido = product.cost <= precioMax;
+    }
+
+    //devuelve true si cumple con ambos filtros
+    return precioMinEsValido && precioMaxEsValido;
+    });
+
+    showProductsList(productosFiltrados);
+
+})
+
+//ordenar por precio ascendente
+document.getElementById('filtrarPrecioAsc').addEventListener('click', ()=>{
+    let filtrarProductos = [...productsArray].sort((a, b) => a.cost - b.cost);
+    console.log(filtrarProductos);
+    
+    showProductsList(filtrarProductos)
+})
+
+//ordenar por precio descendiente
+document.getElementById('filtrarPrecioDesc').addEventListener('click', () =>{
+    let filtrarProductos = [...productsArray].sort((a, b) => b.cost - a.cost);
+    console.log(filtrarProductos);
+    showProductsList(filtrarProductos);
+})
+
+//filtrar por relevancia
+document.getElementById('filtrarPorRelevancia').addEventListener('click', ()=>{
+    let filtrarProductos = [...productsArray].sort((a, b) => b.soldCount - a.soldCount);
+    console.log(filtrarProductos);
+    showProductsList(filtrarProductos);
+    
+})
+
+//boton que limpia el filtro
+document.getElementById('limpiarFiltro').addEventListener('click', () =>{
+    document.getElementById('precioMin').value = "";
+    document.getElementById('precioMax').value = "";
+    showProductsList(productsArray); //vuelve a mostrar todos los productos sin el filtro aplicado
 });
 
+//buscar productos en tiempo real
+            let searchInput = document.getElementById('searchInput');
+            searchInput.addEventListener('input', function () {
+                let searchTerm = searchInput.value.toLowerCase();
+                let productosFiltrados = productsArray.filter(product => {
+                    return product.name.toLowerCase().includes(searchTerm) ;
+                });
+                showProductsList(productosFiltrados);
+            });
+
+} else { console.log("Error de productos"); }
+
+    });
+
+
 function showProductsList(array) {
-  let htmlContentToAppend = "";
-  for (let products of array) {
-    htmlContentToAppend += `
-         <div class="col-12 col-sm-6 col-md-6 col-lg-4 mb-4">
+    let htmlContentToAppend = "";
+    for (let products of array) {
+
+        htmlContentToAppend += `
+         <div class="col-md-4">
                 <div class="card mb-4 shadow-sm">
                     <img src="${products.image}" class="bd-placeholder-img card-img-top" alt="${products.name}">
                     <div class="card-body">
@@ -30,6 +100,10 @@ function showProductsList(array) {
                     </div>
                 </div>
             </div>`;
-    container.innerHTML = htmlContentToAppend;
-  }
+        container.innerHTML = htmlContentToAppend;
+    }
 }
+
+
+
+});
