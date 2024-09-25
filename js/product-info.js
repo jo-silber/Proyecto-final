c38c5641824325f74c6909987e7758debe05b67b
emi <emilycabral185@gmail.com>
2024-09-25 12:32:38 -0300

    estilos pord-info

--- js/product-info.js
+++ js/product-info.js
@@ -1,52 +1,46 @@
+document.addEventListener("DOMContentLoaded", function (e) {
+  let productID = localStorage.getItem("productID");
+  let products = [];
 
+  let url = PRODUCT_INFO_URL + productID + EXT_TYPE; //url para obetener la informacion del producto
 
-document.addEventListener('DOMContentLoaded', function (e){
-    let productID = localStorage.getItem("productID");
-    let productsArray = [];
+  getJSONData(url).then(function (resultObj) {
+    if (resultObj.status === "ok") {
+      products = resultObj.data;
 
-    let url =  PRODUCT_INFO_URL + productID + EXT_TYPE //url para obetener la informacion del producto
+      showInfoProducto(products);
+    } else {
+      console.error(
+        "No se encontró ningún productID en el almacenamiento local."
+      );
+    }
 
-    getJSONData(url).then(function (resultObj) {
-        if(resultObj.status === "ok"){
-            products = resultObj.data;
-        
+    function showInfoProducto(products) {
+      console.log(products);
 
-            showInfoProducto(productsArray);
-        } else {
-            console.error("No se encontró ningún catID en el almacenamiento local.");
-        }
+      let htmlContentToAppend = "";
 
-        function showInfoProducto(productsArray){
-            console.log(products);
-
-            let htmlContentToAppend = "";
-            
-         
-
-                htmlContentToAppend += `
-                <p><strong>Categoria:</strong> ${products.category}</p> 
-            <div class="col-md-6">
+      htmlContentToAppend += `
+                 
+                <div class="row">
+            <div class="col-md-6" style="margin-top:50px;">
            <div class="product-images">`;
 
-            for (let i = 0; i < products.images.length; i++) {
-                let imageSrc = products.images[i]; 
-                htmlContentToAppend += `
-                    <img src="${imageSrc}" alt="Imagen del producto" class="img-thumbnail"/ >  `;
-            }
+      for (let i = 0; i < products.images.length; i++) {
+        let imageSrc = products.images[i];
+        htmlContentToAppend += `
+                    <img id="imgInfo" src="${imageSrc}" alt="Imagen del producto" / >  `;
+      }
 
-           
+      htmlContentToAppend += ` </div> </div><div id="prodDes"class="col-md-6">
+                <h1 class="prodName">${products.name}</h1>
+                <p><strong>Descripción: </strong> ${products.description}</p>
+                <p><strong id="product-price">Precio:</strong>${products.currency} ${products.cost}</p>
+                <p> <strong id="sold-count">Vendidos: </strong> ${products.soldCount}  </p>
+                <p><strong>Categoria:</strong> ${products.category}</p>
+            </div> </div> </div> `;
 
-
-             htmlContentToAppend += `
-                <h1>${products.name}</h1>
-                <p> <strong id="sold-count"> ${products.soldCount} Vendidos </strong> </p>
-                <p><strong id="product-price">${products.currency} ${products.cost}</strong> </p>
-                <p> ${products.description}</p>
-            </div> </div> </div>`;
-            
-
-            document.getElementById('product-info').innerHTML = htmlContentToAppend;
-            
-        }
-    })
-})
+      document.getElementById("product-info").innerHTML = htmlContentToAppend;
+    }
+  });
+});
