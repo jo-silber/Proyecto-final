32bcbf5cafdbeb9c5fe2295b0d7b3b4a2828e32f
emi <emilycabral185@gmail.com>
2024-10-01 19:33:37 -0300

    ahora si el boton

--- js/product-info.js
+++ js/product-info.js
@@ -1,11 +1,18 @@
+function setProductId(id) {
+    localStorage.setItem("productID", id);
+    window.location = "product-info.html";
+  }
+
 document.addEventListener("DOMContentLoaded", function (e) {
   let productID = localStorage.getItem("productID");
   let products = [];
 
   let url = PRODUCT_INFO_URL + productID + EXT_TYPE; //url para obetener la informacion del producto
 
+
+
   getJSONData(url).then(function (resultObj) {
-    if (resultObj.status === "ok") {
+    if (resultObj.status === "ok") {    `1  QZ`
       products = resultObj.data;
 
       showInfoProducto(products);
@@ -21,7 +28,6 @@ document.addEventListener("DOMContentLoaded", function (e) {
       let htmlContentToAppend = "";
 
       htmlContentToAppend += `
-                 
                 <div class="row">
             <div class="col-md-6" style="margin-top:50px;">
            <div class="product-images">`;
@@ -39,6 +45,32 @@ document.addEventListener("DOMContentLoaded", function (e) {
                 <p> <strong id="sold-count">Vendidos: </strong> ${products.soldCount}  </p>
                 <p><strong>Categoria:</strong> ${products.category}</p>
             </div> </div> </div> `;
+
+
+           // productos relacionados
+      htmlContentToAppend += `
+        <div class="row mt-5">
+            <h2>Productos Relacionados</h2>
+        </div>
+        <div class="row" id="related-products">`;
+
+      for (let i = 0; i < products.relatedProducts.length; i++) {
+        let relatedProduct = products.relatedProducts[i];
+        htmlContentToAppend += `
+            <div class="col-md-3">
+                <div class="card mb-3">
+                    <img src="${relatedProduct.image}" ">
+                    <div class="card-body">
+                        <h5 >${relatedProduct.name}</h5>
+                        <button onclick="setProductId(${relatedProduct.id})"  class="btn btn-primary">Ver producto</button>
+                    </div>
+                </div>
+            </div> `;
+    
+      }
+
+      htmlContentToAppend += `</div>`; 
+            
 
       document.getElementById("product-info").innerHTML = htmlContentToAppend;
     }
